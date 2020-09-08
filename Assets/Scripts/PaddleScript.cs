1618b82d9b0a9ef4ecaa71d322d0333f5d86d521
M Bethancourt <matt@mouseandthebillionaire.com>
2020-09-08 15:44:56 -0600

    Initial state of Breakin' project

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+    private float     xPos;
+    public float      paddleSpeed = .05f;
+    public float      leftWall, rightWall;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            if (xPos > leftWall) {
+                xPos -= paddleSpeed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            if (xPos < rightWall) {
+                xPos += paddleSpeed;
+            }
+        }
+
+        transform.localPosition = new Vector3(xPos, transform.position.y, 0);
+    }
+}
+
