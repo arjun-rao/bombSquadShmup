e01b30ff34619c2b00cb7d0a194a01d96be7d2e1
M Bethancourt <matt@mouseandthebillionaire.com>
2020-09-22 22:41:35 -0600

    A few tweaks - prep for Shmupness

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public float               speed = 2f;
+    
+    private Rigidbody2D        rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("Launch");
+    }
+
+    
+    private IEnumerator Launch() {
+        //yield return new WaitForSeconds(1);
+        //rb.AddForce(transform.right * -1);
+        rb.AddForce(transform.up * speed);
+        yield return null;
+    }
+}
